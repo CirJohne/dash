883f6e0fd839c5592a1a8c3cba52ab6871624052
CirJohne <john.cirverius@gmail.com>
2020-02-11 07:35:46 +0100

    lesson3 in progress

---
+++ lesson03/lesson03.tests/Tests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Shouldly;
+using Xunit;
+
+namespace lesson03.tests
+{
+    public class Tests
+    {
+
+        [Fact]
+        public void RotateArray()
+        {
+            var rotator = new Rotator();
+            rotator.Rotate(new int[] {1,2,3,4}).ShouldBe(new int[] {2,3,4,1});
+            rotator.Rotate(new int[] {4,5,6,7}).ShouldBe(new int[] {5,6,7,4});
+        }
+       
+        [Fact]
+        public void SmallestPartOfArrays()
+        {
+            var finder = new Finder();
+            var firstArr = new int[]{5,33,1,8,3,2};
+            var secondArr = new int[]{23,5454,2,3,745};
+            var thirdArr = new int[]{45,76,4,878,3};
+            var result = new int[]{1,2,3};
+            finder.Smallest(firstArr, secondArr, thirdArr).ShouldBe(result);
+        }
+
+        [Fact]
+        public void IsPalindrome()
+        {
+            var palindrome = new Palindrome();
+            palindrome.IsPalindrome("hejsan").ShouldBe(false);
+            palindrome.IsPalindrome("anna").ShouldBe(true);
+            palindrome.IsPalindrome("Madam I'm Adam").ShouldBe(true);
+        }
+
+        [Fact]
+        public void SortArrayOfObjects()
+        {
+            
+            var sorter = new Sorter();
+            var stefan = new TeamMember("Stefan","Champagne", 39);
+            var johne = new TeamMember("Johne", "Whisky Sour", 39);
+            var daria = new TeamMember("Daria", "Shots", 28);
+            var niklas = new TeamMember("Niklas", "Beer", 41);
+            var ulrika = new TeamMember("Ulrika", "Red Wine", 38);
+            var soleman = new TeamMember("Soleman", "Sangria", 35);
+
+
+            var arr = new TeamMember[] {stefan, johne};
+            sorter.SortArrayOfObjectsByAge(arr).ShouldBe(new TeamMember[]{johne, stefan });
+        }
+    }
+}
---
+++ lesson03/lesson03/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace lesson03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Rotator
+    {
+        public int[] Rotate(int[] arr)
+        {
+            return arr;
+        }
+    }
+
+    public class Finder
+    {
+        public int[] Smallest(int[] arr1, int[] arr2, int[] arr3)
+        {
+            return arr1;
+        }
+    }
+
+    public class Palindrome
+    {
+        public bool IsPalindrome(string word)
+        {
+            return true;
+        }
+    }
+
+    public class Sorter
+    {
+        public TeamMember[] SortArrayOfObjectsByAge(TeamMember[] arr)
+        {
+            return arr;
+        }
+    }
+    public class TeamMember
+    {
+        string name {get; set;}
+        string drink {get; set;}
+        int age {get; set;}
+
+        public TeamMember(string name, string drink, int age)
+        {
+            this.name = name;
+            this.drink = drink;
+            this.age = age;
+        }
+    }
+  
+}
+ 
+
+
+
